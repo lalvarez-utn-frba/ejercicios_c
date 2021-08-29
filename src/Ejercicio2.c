0364071a6c199665bd56b3876d8e49e8cbbae112
joaquinipar <joaquin.ipar@gmail.com>
2021-08-29 17:45:14 -0300

    Agrego ejercicio2 completo leak free

--- src/Ejercicio2.c
+++ src/Ejercicio2.c
@@ -12,10 +12,12 @@
 
 #include "archivoC.h"
 
+
 int main(void) {
 
-	char* ruta = "/home/utnso/Documentos/Ejercicio2/archivazo.txt";
+	//char* ruta = "/home/utnso/Documentos/Ejercicio2/archivazo.txt";
 
+	/*
 	archivo nuevo_archivo = abrir_archivo(ruta, READ);
 
 	char* nueva_linea = leer_linea(nuevo_archivo,2);
@@ -23,6 +25,62 @@ int main(void) {
 	cerrar_archivo(&nuevo_archivo);
 
 	free(nueva_linea);
+	*/
+
+	/*
+	char* nombre = string_new();
+	char* saludo = "Hola!!!;";
+
+	strcpy(nombre, "Joaquin");
+	strcat(nombre, "\0");
+
+	puts(appendSaludo(nombre, saludo));
+
+*/
+
+
+	/* prueba de aplicar_funcion_archivo */
+	/*
+	char* ruta = "/home/utnso/Documentos/Ejercicio2/archivazo.txt";
+
+
+	archivo nuevo_archivo = abrir_archivo(ruta, READ);
+
+
+	aplicar_funcion_archivo(&pasameAMinuscula, nuevo_archivo );
+
+	cerrar_archivo(&nuevo_archivo);
+	*/
+
+
+	char* ruta = "/home/utnso/Documentos/Ejercicio2/archivazo.txt";
+	archivo nuevo_archivo = abrir_archivo(ruta, APPEND);
+
+	// Prueba de creacion de lista
+
+	t_list* miListaDeComidas = list_create();
+
+	// Ahora quiero crear un elemento para agregar a mi lista :)
+
+	list_add(miListaDeComidas, "PIZZA");
+	list_add(miListaDeComidas, "HAMBURGUESA");
+	list_add(miListaDeComidas, "ASADO");
+	list_add(miListaDeComidas, "ARROZ");
+	list_add(miListaDeComidas, "FIDEO");
+
+	aplicar_func_lista_en_file(miListaDeComidas, &string_reverse, nuevo_archivo);
+	//list_iterate(miListaDeComidas, &puts);
+	puts("hola");
+
+	list_destroy(miListaDeComidas);
+	cerrar_archivo(&nuevo_archivo);
+
+	puts("hola");
 
 	return 0;//EXIT_SUCCESS;
 }
+
+
+
+
+
--- src/archivo.c
+++ src/archivo.c
@@ -1,6 +1,5 @@
 #include "archivoC.h"
 
-
 archivo abrir_archivo (char* path, modo_lectura modo){ // return 0 OK! ; return -1 ERROR! abriendo el archivo
 
 	archivo nuevo_archivo;
@@ -81,13 +80,177 @@ char* leer_linea(archivo archivo, int numero_linea){
 	   return linea;
 }
 
+/*
+ *
+ * exponer otra operación que dada una lista y una función,
+ * itere la lista y escriba sobre el archivo abierto lo
+ *  que devuelve dicha función (string).
+ *
+ */
+
+
+void destructorDeComidas(t_link_element* elem){
+
+	printf("Destruyendo: %s",(char*) (elem->data) );
+	puts( (char*) (elem->data) );
+	free(elem->data);
+	free(elem);
+
+}
+
+void aplicar_func_lista_en_file ( t_list* lista, char* (*funcion)( char*), archivo archivo ){
+
+	if(archivo.esta_abierto == 1 && archivo.modo == APPEND){
+
+		t_list* nueva_lista; // no malloqueo, se encarga list_map
+
+
+
+		nueva_lista = list_map(lista, (void*) funcion); // me devuelve la lista iterada
+
+		puts("hola");
+
+		t_list_iterator* iterador = list_iterator_create(nueva_lista);
+
+		list_iterator_next(iterador);
+
+		while(list_iterator_has_next(iterador)){
+
+			puts((char*) (iterador->element->data));
+			fwrite( (char*) (iterador->element->data), sizeof(char), strlen(iterador->element->data), archivo.puntero_archivo);
+
+			list_iterator_next(iterador);
+		}
+
+
+
+		/*
+		t_link_element* sig;
+		sig = nueva_lista->head;
+
+		while( sig->next != NULL ){
+		//fwrite(nueva_lista->head, sizeof(nueva_lista->head), archivo.puntero_archivo);
+			puts((char*) (sig->data));
+			fwrite( (char*) (sig->data), sizeof(char), strlen(sig->data), archivo.puntero_archivo);
+
+			sig->data = sig->next->data;
+			sig-> next = (sig->next)->next;
+
+			printf("proximo %s:", sig->data);
+
+			if( sig->next == NULL){
+
+				puts((char*) (sig->data));
+				fwrite( (char*) (sig->data), sizeof(char), strlen(sig->data), archivo.puntero_archivo);
+
+				}
+
+
+			}
+	*/
+
+
+		list_destroy_lista_con_contenido(nueva_lista);
+		list_iterator_destroy(iterador);
+	//list_destroy_and_destroy_elements(&nueva_lista, (void*) destructorDeComidas);
+	//list_destroy(nueva_lista); // libero la memoria de nueva_lista ( list_map hizo malloc)
+	//free(sig->next);
+	//free(sig->data);
+	//free(sig);
+		}
+
+}
+
+void list_destroy_lista_con_contenido(t_list *self) {
+	list_clean_contenidos(self);
+	free(self);
+}
+
+void list_clean_contenidos(t_list *self) {
+	t_link_element* elemento;
+	while (self->head != NULL) {
+		elemento = self->head;
+		self->head = self->head->next;
+		free(elemento->data);
+		free(elemento);
+	}
+	self->elements_count = 0;
+}
+
+void llenarLista(t_list* lista){
+
+	list_add(lista, "PIZZA");
+	list_add(lista, "HAMBURGUESA");
+	list_add(lista, "ASADO");
+	list_add(lista, "ARROZ");
+	list_add(lista, "FIDEO");
+
+
+
+}
 
 
 /*
+ *  exponer una operación que reciba una función
+ *  y la ejecute por cada línea del archivo
+ *
+ */
+
+void aplicar_funcion_archivo( char* (*funcion)(char*), archivo archivo){
+
+	puts("Estoy en aplicar_funcion_archivo");
+
+	char* linea = 0;
+	size_t tamanio = 0;
+	ssize_t bytes_leidos = 0;
+	int contador = 0;
+	//char* temp = string_new();
+	//char* resultado = string_new();
+
+	while( (bytes_leidos = getline(&linea, &tamanio, archivo.puntero_archivo) ) != -1 ){
+
+		/*
+		char* saludo = malloc(sizeof(char*));
+
+		strcpy(saludo, "Hola!!!");
+
+		string_append(&linea);
+
+		strcat(linea, "\0");
+*/
+
+		funcion(linea);
+
+		//strcat(linea, "\0");
+
+		puts(linea);
+
+		//free(saludo);
+
+		contador++;
+	}
+
+	free(linea);
+
+}
+
+void pasameAMinuscula(char* original){
+
+
+		//string_append(&original, "HOLA!!!");
+
+		//printf("%s", original);
+
+		string_to_lower(original);
+
+	}
+
+
+
 void append_char( char* s,  char c) {
-        int len = strlen(s)
+        int len = strlen(s);
         s[len] = c;
         s[len+1] = '\0';
 }
-*/
+
 
