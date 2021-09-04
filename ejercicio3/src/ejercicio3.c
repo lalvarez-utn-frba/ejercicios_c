efde2451902e5ae92fe49a085ca582d759d14479
Luis Alvarez <lalvarez@frba.utn.edu.ar>
2021-09-04 00:35:41 -0300

    Validacion de archivos antes de procesarlos

--- ejercicio3/src/ejercicio3.c
+++ ejercicio3/src/ejercicio3.c
@@ -9,12 +9,26 @@
  */
 
 #include "procesar_archivos.h"
+#include <unistd.h>
+#include <limits.h>
 
 int main(void) {
 	puts("!!!Procesando archivo!!!");
 
-	FILE* archivoEntrada = fopen("/home/utnso/ejercicios_c/ejercicio3/personas.txt", "r+");
-	FILE* archivoSalida = fopen("/home/utnso/ejercicios_c/ejercicio3/personasOrdenada.txt", "w+");
+	FILE* archivoEntrada = fopen("./personas.txt", "r+");
+	FILE* archivoSalida = fopen("./personasOrdenada.txt", "w+");
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	printf("Directorio de trabajo: %s\n", cwd);
+
+	if (archivoEntrada == NULL) {
+		perror("Error al abrir el archivo: ");
+		printf("No se pudo abrir el archivo.\nComprueba si el archivo (personas.txt) existe en el path indicado y si tiene los permisos apropiados.\n");
+		printf("Si el directorio de trabajo es /Debug (al correrlo en la terminal), podes llamar a la aplicacion desde la carpeta ejercicios_c/ejercicio3, escribiendo Debug/ejercicio3\n");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 	procesar_archivos(archivoEntrada, archivoSalida);
 
 	fclose(archivoEntrada);
